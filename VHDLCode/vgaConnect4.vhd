86059267add143a0f3e9f9e19b95c7e4d667b93d
Pablo C. Ruíz <pcruiher089@gmail.com>
2018-12-03 20:47:27 -0600

    added no trimmed signals

--- VHDLCode/vgaConnect4.vhd
+++ VHDLCode/vgaConnect4.vhd
@@ -324,33 +324,33 @@ if(columna = 1 and botonAb = '1' and (not columna1General = "1111"))then
     end if;
     if(columna3General = "0001") then
         if(jugador = '1') then
-            columna3Rojo(0 to 2)<="001";
+            columna3Rojo(2 downto 0)<="001";
             jugador<='0';
             columna3General <= "0011"; 
         else
-            columna3Amarillo(0 to 2)<="001";
+            columna3Amarillo(0 downto 0)<="001";
             jugador<='1';
             columna3General <= "0011"; 
         end if;
     end if;
     if(columna3General = "0011") then
         if(jugador = '1') then
-            columna3Rojo(0 to 1)<="01";
+            columna3Rojo(1 downto 0)<="01";
             jugador<='0';
             columna3General <= "0111"; 
         else
-            columna3Amarillo(0 to 1)<="01";
+            columna3Amarillo(1 downto 0)<="01";
             jugador<='1';
             columna3General <= "0111"; 
         end if;
     end if;
     if(columna3General = "0111") then
         if(jugador = '1') then
-            columna3Rojo(0 to 0)<="1";
+            columna3Rojo(0 downto 0)<="1";
             jugador<='0';
             columna3General <= "1111"; 
         else
-            columna3Amarillo(0 to 0)<="1";
+            columna3Amarillo(0 downto 0)<="1";
             jugador<='1';
             columna3General <= "1111"; 
         end if;
@@ -371,33 +371,33 @@ if(columna = 2 and botonAb = '1' and (not columna2General = "1111"))then
     end if;
     if(columna2General = "0001") then
         if(jugador = '1') then
-            columna2Rojo(0 to 2)<="001";
+            columna2Rojo(2 downto 0)<="001";
             jugador<='0';
             columna2General <= "0011"; 
         else
-            columna2Amarillo(0 to 2)<="001";
+            columna2Amarillo(2 downto 0)<="001";
             jugador<='1';
             columna2General <= "0011"; 
         end if;
     end if;
     if(columna2General = "0011") then
         if(jugador = '1') then
-            columna2Rojo(0 to 1)<="01";
+            columna2Rojo(1 downto 0)<="01";
             jugador<='0';
             columna2General <= "0111"; 
         else
-            columna2Amarillo(0 to 1)<="01";
+            columna2Amarillo(1 downto 0)<="01";
             jugador<='1';
             columna2General <= "0111"; 
         end if;
     end if;
     if(columna2General = "0111") then
         if(jugador = '1') then
-            columna2Rojo(0 to 0)<="1";
+            columna2Rojo(0 downto 0)<="1";
             jugador<='0';
             columna2General <= "1111"; 
         else
-            columna2Amarillo(0 to 0)<="1";
+            columna2Amarillo(0 downto 0)<="1";
             jugador<='1';
             columna2General <= "1111"; 
         end if;
@@ -418,33 +418,33 @@ if(columna = 3 and botonAb = '1' and (not columna3General = "1111"))then
     end if;
     if(columna3General = "0001") then
         if(jugador = '1') then
-            columna3Rojo(0 to 2)<="001";
+            columna3Rojo(2 downto 0)<="001";
             jugador<='0';
             columna3General <= "0011"; 
         else
-            columna3Amarillo(0 to 2)<="001";
+            columna3Amarillo(2 downto 0)<="001";
             jugador<='1';
             columna3General <= "0011"; 
         end if;
     end if;
     if(columna3General = "0011") then
         if(jugador = '1') then
-            columna3Rojo(0 to 1)<="01";
+            columna3Rojo(1 downto 0)<="01";
             jugador<='0';
             columna3General <= "0111"; 
         else
-            columna3Amarillo(0 to 1)<="01";
+            columna3Amarillo(1 downto 0)<="01";
             jugador<='1';
             columna3General <= "0111"; 
         end if;
     end if;
     if(columna3General = "0111") then
         if(jugador = '1') then
-            columna3Rojo(0 to 0)<="1";
+            columna3Rojo(0 downto 0)<="1";
             jugador<='0';
             columna3General <= "1111"; 
         else
-            columna3Amarillo(0 to 0)<="1";
+            columna3Amarillo(0 downto 0)<="1";
             jugador<='1';
             columna3General <= "1111"; 
         end if;
@@ -466,12 +466,12 @@ if(columna = 4 and botonAb = '1' and (not columna4General = "1111"))then
     if(columna4General = "0001") then
         if(jugador = '1') then
             --columna4Rojo(2)<='1';
-            columna4Rojo(0 to 2)<="001";
+            columna4Rojo(2 downto 0)<="001";
             jugador<='0';
             columna4General <= "0011"; 
         else
             --columna4Amarillo(2)<='1';
-            columna4Amarillo(0 to 2)<="001";
+            columna4Amarillo(2 downto 0)<="001";
             --"----" , "--1-", "001-"
             jugador<='1';
             columna4General <= "0011"; 
@@ -480,12 +480,12 @@ if(columna = 4 and botonAb = '1' and (not columna4General = "1111"))then
     if(columna4General = "0011") then
         if(jugador = '1') then
             --columna4Rojo(1)<='1';
-            columna4Rojo(0 to 1)<="01";
+            columna4Rojo(1 downto 0)<="01";
             jugador<='0';
             columna4General <= "0111"; 
         else
             --columna4Amarillo(1)<='1';
-            columna4Amarillo(0 to 1)<="01";
+            columna4Amarillo(1 downto 0)<="01";
             jugador<='1';
             columna4General <= "0111"; 
         end if;
@@ -493,12 +493,12 @@ if(columna = 4 and botonAb = '1' and (not columna4General = "1111"))then
     if(columna4General = "0111") then
         if(jugador = '1') then
             --columna4Rojo(0)<='1';
-            columna4Rojo(0 to 0)<="1";
+            columna4Rojo(0 downto 0)<="1";
             jugador<='0';
             columna4General <= "1111"; 
         else
             --columna4Amarillo(0)<='1';
-            columna4Amarillo(0 to 0)<="1";
+            columna4Amarillo(0 downto 0)<="1";
             jugador<='1';
             columna4General <= "1111"; 
         end if;
